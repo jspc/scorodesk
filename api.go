57207d10c709cbec4f7dafd46ae3b5a8e067593f
jspc <james@zero-internet.org.uk>
2017-07-09 17:18:05 +0100

    Receive data from zendesk

--- api.go
+++ api.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-	_ "fmt"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type IncomingRequest struct {
+	// project title; taken from the email subject
+	Title string
+
+	// email address of the requestor
+	Requestor string
+
+	// taken from the body of the original ticket
+	Description string
+}
+
 func Incoming(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	decoder := json.NewDecoder(r.Body)
+	sr := IncomingRequest{}
+
+	err := decoder.Decode(&sr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadGateway)
+
+		ErrorLogger.Print(err)
+		fmt.Fprint(w, err)
+
+		return
+	}
+
+	defer r.Body.Close()
+
 }
